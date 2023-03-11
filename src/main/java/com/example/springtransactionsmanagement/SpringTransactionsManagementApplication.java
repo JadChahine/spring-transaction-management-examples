bca6c448dd115ccf373bd2d7853259865bb4e654
Jad Chahine <jad.chahine@sword-group.com>
2023-03-11 22:55:32 +0200

    Initialize the spring boot project skeleton

---
+++ src/main/java/com/example/springtransactionsmanagement/SpringTransactionsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtransactionsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTransactionsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTransactionsManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springtransactionsmanagement/SpringTransactionsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtransactionsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransactionsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
